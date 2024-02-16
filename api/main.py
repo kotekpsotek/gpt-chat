8cff838eafc97794f637e0e018482b8925274889
kotekpsotek <siematumichael@protonmail.com>
2024-02-16 05:16:04 +0100

    Refined some

--- api/main.py
+++ api/main.py
@@ -1,12 +1,12 @@
 from flask import Flask, jsonify, request
 from time import sleep
-from .llm.main import gpt_2_neo
+from llm.main import gpt_2_neo
 
 app = Flask(__name__, template_folder="static")
 
 @app.post("/question")
 def question():
-    question = request.json
+    question = request.json['question']
 
     if len(question):
         answer = gpt_2_neo(question) # Fill this field
@@ -15,7 +15,7 @@ def question():
             "answer": answer
         }), 200
     else:
-        return 400
+        return "", 400
 
 if __name__ == "__main__":
     app.run(port=5454, debug=True)
