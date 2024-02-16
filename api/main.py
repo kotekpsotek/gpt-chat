de7824859e7ea8e6f8b2a90c1172bd93bfa7a696
kotekpsotek <siematumichael@protonmail.com>
2024-02-16 05:29:23 +0100

    CORS Policy Attached to server

--- api/main.py
+++ api/main.py
@@ -1,11 +1,16 @@
 from flask import Flask, jsonify, request
+from flask_cors import CORS, cross_origin
 from time import sleep
 from llm.main import gpt_2_neo
 
 app = Flask(__name__, template_folder="static")
+cors = CORS(app)
+app.config['CORS_HEADERS'] = 'Content-Type'
 
 @app.post("/question")
+@cross_origin()
 def question():
+    print(request.json)
     question = request.json['question']
 
     if len(question):
