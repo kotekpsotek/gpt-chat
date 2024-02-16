8d5552bcb16a75c710b38619dcc205f21c186600
kotekpsotek <siematumichael@protonmail.com>
2024-02-16 04:59:47 +0100

    AI GPT Model implemented

---
+++ api/llm/main.py
@@ -0,0 +1,15 @@
+from transformers import pipeline
+
+def process_gpt_2_neo(prompt: str):
+    # Load pipeline
+    generator = pipeline('text-generation', model='EleutherAI/gpt-neo-1.3B')
+    
+    # Generate answer
+    answer = generator(prompt, do_sample=True, max_length=400, temperature=0.9, truncation=True)
+
+    # Streamline answer
+    return answer[0]["generated_text"]
+
+if __name__ == "__main__":
+    answer = process_gpt_2_neo("What is the differenece between Lockeed Martin and Boeing?")
+    print(answer)
