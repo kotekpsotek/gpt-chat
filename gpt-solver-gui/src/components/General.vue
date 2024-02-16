082ad141a451c444550a36c2d9984dc6978e4d54
kotekpsotek <siematumichael@protonmail.com>
2024-02-16 02:51:51 +0100

    Basic GUI APP

---
+++ gpt-solver-gui/src/components/General.vue
@@ -0,0 +1,44 @@
+<script setup lang="ts">
+    import { ref, defineProps } from 'vue';
+
+    let myId = "desc";
+
+    const question = ref("");
+    const answer = ref("");
+
+    // define props using a type literal
+    const props = defineProps({
+        api: {
+            type: String,
+            required: true
+        }
+    })
+
+    async function sendQuestion() {
+        const f = await fetch(props.api, {
+            method: "POST",
+            headers: {
+                'content-type': "application/json"
+            },
+            body: JSON.stringify({ question })
+        });
+
+        if (f.status == 200) {
+            const { answer: a } = (await f.json())
+            answer.value = a
+        }
+        else alert("Cannot make request for datas!");
+    }
+</script>
+
+<template>
+    <div>
+        <h1>Chat with GPT</h1>
+        <p :id="myId">I'm happy to let you clear a unclear!</p>
+        <input type="text" placeholder="How can i help you?" v-model="question">
+        <button @click="sendQuestion">Ask</button>
+    </div>
+    <div class="answer" v-if="answer.length">
+        <p>{{ answer }}</p>
+    </div>
+</template>
