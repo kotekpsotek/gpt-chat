f051f45c523b7fdfab2f2566fbf9030e54137428
kotekpsotek <siematumichael@protonmail.com>
2024-02-17 01:48:10 +0100

    Chat GUI with tailwind-css

--- gpt-solver-gui/src/components/General.vue
+++ gpt-solver-gui/src/components/General.vue
@@ -1,10 +1,13 @@
 <script setup lang="ts">
     import { ref, defineProps } from 'vue';
+    import Answering from "../assets/answering.svg";
+    import BotProfile from "../assets/bot_profile.jpg";
 
     let myId = "desc";
 
     const question = ref("");
-    const answer = ref("");
+    const answer = ref("Hi there");
+    const durningAnswering = ref(false);
 
     // define props using a type literal
     const props = defineProps({
@@ -15,34 +18,64 @@
     })
 
     async function sendQuestion() {
-        const apiUrl = new URL(props.api);
-        apiUrl.pathname = "/question"
-        console.log(apiUrl.toString())
-        
-        const f = await fetch(apiUrl.toString(), {
-            method: "POST",
-            headers: {
-                'content-type': "application/json"
-            },
-            body: JSON.stringify({ question: question.value })
-        });
-
-        if (f.status == 200) {
-            const { answer: a } = (await f.json())
-            answer.value = a
+        try { 
+            durningAnswering.value = true;
+            answer.value = "";
+    
+            const apiUrl = new URL(props.api);
+            apiUrl.pathname = "/question"
+            console.log(apiUrl.toString())
+            
+            const f = await fetch(apiUrl.toString(), {
+                method: "POST",
+                headers: {
+                    'content-type': "application/json"
+                },
+                body: JSON.stringify({ question: question.value })
+            });
+    
+            if (f.status == 200) {
+                const { answer: a } = (await f.json())
+                answer.value = a
+            }
+            else alert("Cannot make request for datas!");
+    
+            durningAnswering.value = false;
         }
-        else alert("Cannot make request for datas!");
+        catch(err) {
+            durningAnswering.value = false;
+        }
     }
 </script>
 
 <template>
-    <div>
-        <h1>Chat with GPT</h1>
-        <p :id="myId">I'm happy to let you clear a unclear!</p>
-        <input type="text" placeholder="How can i help you?" v-model="question">
-        <button @click="sendQuestion">Ask</button>
-    </div>
-    <div class="answer" v-if="answer.length">
-        <p>{{ answer }}</p>
+    <div class="wrapper w-screen h-screen flex flex-col gap-4 justify-center items-center">
+        <div class="card">
+            <h1 class="text-2xl font-semibold">Chat with GPT</h1>
+            <p :id="myId" class="font-protestRiot text-gray-500">I'm happy to let you clear a unclear!</p>
+            <div id="trigger" class="flex flex-col gap-2 mt-3">
+                <textarea id="question" class="bg-slate-200 p-2 rounded-sm" style="resize: none;" placeholder="How do you feel?" cols="30" rows="10" wrap="soft" v-model="question">
+                </textarea>
+                <button @click="sendQuestion" class="w-full p-2 text-black font-semibold border-2 border-black rounded-md hover:bg-black hover:text-white transition-all duration-150">Ask</button>
+            </div>
+        </div>
+        <div v-if="durningAnswering" class="flex gap-x-2">
+            <p>Answering</p>
+            <img v-bind:src="Answering" alt="Loading...">
+        </div>
+        <div class="card" v-if="answer.length">
+            <div class="flex items-start justify-start">
+                <img :src="BotProfile" class="rounded-full" width="40px" height="40px">
+                <div id="answer" class="flex items-center overflow-y-auto" style="max-height: 200px;">
+                    <p>{{ answer }}</p>
+                </div>
+            </div>
+        </div>
     </div>
 </template>
+
+<style>
+    .card {
+        @apply bg-white p-4 rounded-md w-3/5 border border-solid border-black;
+    }
+</style>
---
+++ gpt-solver-gui/tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./src/**/*.{html,js,jsx,tsx,vue}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        'inter': ["Inter", "sans-serif"],
+        'protestRiot': '"Protest Riot", sans-serif'
+      }
+    },
+  },
+  plugins: [],
+}
+
