b89746dea480ce9dd0fff3a77e255804ed185679
kotekpsotek <siematumichael@protonmail.com>
2024-02-17 02:13:43 +0100

    Persistant Storage for Questions API

---
+++ gpt-solver-gui/src/api/index.ts
@@ -0,0 +1,47 @@
+interface Question {
+    content: string,
+    date_timestamp: number
+}
+
+type QuestionList = Question[];
+
+interface QuestionsStorage {
+    questions: QuestionList
+}
+
+export default class API {
+    /**
+     * @description Get all questions from persistant storage
+    */
+    static getStorageQuestions(): QuestionsStorage {
+        const questions = localStorage.getItem("gpt-solver-questions")
+
+        if (!questions) return { questions: [] };
+
+        const deserialized = JSON.parse(questions) as QuestionsStorage;
+
+        return deserialized;
+    }
+
+    /**
+     * @description Save all questions to persisatnt storage
+     * @param questions 
+     * @returns 
+     */
+    static setStorageQuestions(questions: QuestionsStorage): boolean {
+        try {
+            const serialized = JSON.stringify(questions);
+    
+            const _save = localStorage.setItem("gpt-solver-questions", serialized);
+            
+            return true;
+        }
+        catch(err) {
+            // Log
+            console.error(err);
+
+            // .. Return
+            return false;
+        }
+    }
+}
